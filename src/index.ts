09802ddd55bca5834843cef5baecd0794dedbe74
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2024-06-27 21:20:08 -0300

    feat: fetch youtube api

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,23 @@ dotenv.config();
 
 const app = express();
 
-const port = process.env.PORT || 3003;
+const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+app.get('/search/:query', async (req: Request, res: Response) => {
+  try {
+    const query = req.params.query;
+
+    const apiKey = process.env.API_KEY;
+    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${apiKey}`;
+
+    const response = await fetch(url);
+    const json = await response.json();
+
+    return res.send(json).status(200);
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).send('Server error');
+  }
 });
 
 app.listen(port, () => console.log(`Server running at port ${port}`));
